c55ee1eb1b7681bd48f93acdad4d450bbfd9edf7
Yang Zhang <yang@fitle.com>
2016-01-28 15:29:57 +0100

    reformer code

--- app/src/main/java/com/example/yang/openglobj/model/Star.java
+++ app/src/main/java/com/example/yang/openglobj/model/Star.java
@@ -1,32 +1,16 @@
 package com.example.yang.openglobj.model;
 
-import android.opengl.GLES20;
-
-import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
-import static android.opengl.GLES20.glClear;
-import static android.opengl.GLES20.glClearColor;
-import static android.opengl.GLES20.glViewport;
-import static android.opengl.Matrix.multiplyMM;
-import static android.opengl.Matrix.rotateM;
-import static android.opengl.Matrix.setIdentityM;
-import static android.opengl.Matrix.translateM;
-import static android.opengl.GLES20.glFrontFace;
-import static android.opengl.GLES20.glEnable;
-import static android.opengl.GLES20.glCullFace;
-
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
 import java.nio.ShortBuffer;
 
-import javax.microedition.khronos.opengles.GL10;
-
 /**
  * Created by yang on 28/01/16.
  */
 public class Star {
-    static final float X=.525731112119133606f;
-    static final float Z=.850650808352039932f;
+    static final float X = .525731112119133606f;
+    static final float Z = .850650808352039932f;
 
     // The colors mapped to the vertices.
     float[] colors = {
@@ -56,10 +40,10 @@ public class Star {
     }
 
     static short indices[] = new short[]{
-            0,4,1, 0,9,4, 9,5,4, 4,5,8, 4,8,1,
-            8,10,1, 8,3,10, 5,3,8, 5,2,3, 2,7,3,
-            7,10,3, 7,6,10, 7,11,6, 11,0,6, 0,1,6,
-            6,1,10, 9,0,11, 9,11,2, 9,2,5, 7,2,11 };
+            0, 4, 1, 0, 9, 4, 9, 5, 4, 4, 5, 8, 4, 8, 1,
+            8, 10, 1, 8, 3, 10, 5, 3, 8, 5, 2, 3, 2, 7, 3,
+            7, 10, 3, 7, 6, 10, 7, 11, 6, 11, 0, 6, 0, 1, 6,
+            6, 1, 10, 9, 0, 11, 9, 11, 2, 9, 2, 5, 7, 2, 11};
 
 
     public FloatBuffer getVertexBuffer() {
