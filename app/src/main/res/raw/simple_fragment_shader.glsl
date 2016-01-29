077acb01217ddbca084f5158f464962e4caba79f
Yang Zhang <yang@fitle.com>
2016-01-29 12:28:09 +0100

    delete attenuation

--- app/src/main/res/raw/simple_fragment_shader.glsl
+++ app/src/main/res/raw/simple_fragment_shader.glsl
@@ -10,9 +10,6 @@
     // The entry point for our fragment shader.
     void main()
     {
-    	// Will be used for attenuation.
-        float distance = length(u_LightPos - v_Position);
-
     	// Get a lighting direction vector from the light to the vertex.
         vec3 lightVector = normalize(u_LightPos - v_Position);
 
@@ -25,9 +22,6 @@
         } else {
         	diffuse = max(dot(-v_Normal, lightVector), 0.0);
         }
-
-    	// Add attenuation.
-        diffuse = diffuse * (1.0 / (1.0 + (0.10 * distance)));
 
         // Add ambient lighting
         diffuse = diffuse + 0.3;
