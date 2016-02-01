1e706b12ad8954f71e9d24a25e8b02915241e7d0
Yang Zhang <yang@fitle.com>
2016-02-01 11:29:05 +0100

    handle shader program

--- app/src/main/res/raw/avatar_vertex_shader.glsl
+++ app/src/main/res/raw/avatar_vertex_shader.glsl
@@ -5,16 +5,12 @@
     attribute vec3 a_Normal;		// Per-vertex normal information we will pass in.
     attribute vec2 a_TexCoordinate; // Per-vertex texture coordinate information we will pass in.
 
-    varying vec3 v_Position;		// This will be passed into the fragment shader.
     varying vec3 v_Normal;			// This will be passed into the fragment shader.
     varying vec2 v_TexCoordinate;   // This will be passed into the fragment shader.
 
     // The entry point for our vertex shader.
     void main()
     {
-        // Transform the vertex into eye space.
-        v_Position = vec3(u_MVMatrix * a_Position);
-
         // Pass through the texture coordinate.
         v_TexCoordinate = a_TexCoordinate;
 
