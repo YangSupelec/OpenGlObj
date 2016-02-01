97c440a6ab446dd93029a96e2f31ff6d8e12c727
Yang Zhang <yang@fitle.com>
2016-02-01 16:10:05 +0100

    simplifier code

--- app/src/main/java/com/example/yang/openglobj/model/BaseObject3D.java
+++ app/src/main/java/com/example/yang/openglobj/model/BaseObject3D.java
@@ -8,38 +8,12 @@ import java.nio.FloatBuffer;
  * Created by yang on 29/01/16.
  */
 public class BaseObject3D {
-    /**
-     * How many bytes per float.
-     */
+    // useful constantes
     protected final int BYTES_PER_FLOAT = 4;
-    /**
-     * How many bytes per float.
-     */
-    protected final int BYTES_PER_INT = 4;
-
-    /**
-     * Additional constants.
-     */
     protected static final int POSITION_DATA_SIZE_IN_ELEMENTS = 3;
     protected static final int NORMAL_DATA_SIZE_IN_ELEMENTS = 3;
     protected static final int COLOR_DATA_SIZE_IN_ELEMENTS = 4;
     protected static final int TEXCOORD_DATA_SIZE_IN_ELEMENTS = 2;
-
-    public float[] getVertices() {
-        return aVertices;
-    }
-
-    public float[] getTexCoords() {
-        return aTexCoords;
-    }
-
-    public float[] getNormals() {
-        return aNormals;
-    }
-
-    public int[] getIndices() {
-        return aIndices;
-    }
 
     protected float[] aVertices;
     protected float[] aTexCoords;
@@ -51,30 +25,4 @@ public class BaseObject3D {
     protected FloatBuffer normalBuffer;
     protected FloatBuffer textureBuffer;
     protected FloatBuffer colorBuffer;
-
-    protected void assignAttribute (int position, int color, int normal, int texture) {
-        // Pass in the position information
-        GLES20.glVertexAttribPointer(position, POSITION_DATA_SIZE_IN_ELEMENTS, GLES20.GL_FLOAT, false,
-                0, vertexBuffer);
-
-        GLES20.glEnableVertexAttribArray(position);
-
-        // Pass in the color information
-        GLES20.glVertexAttribPointer(color, COLOR_DATA_SIZE_IN_ELEMENTS, GLES20.GL_FLOAT, false,
-                0, colorBuffer);
-
-        GLES20.glEnableVertexAttribArray(color);
-
-        // Pass in the normal information
-        GLES20.glVertexAttribPointer(normal, NORMAL_DATA_SIZE_IN_ELEMENTS, GLES20.GL_FLOAT, false,
-                0, normalBuffer);
-
-        GLES20.glEnableVertexAttribArray(normal);
-
-        // Pass in the texture coordinate information
-        GLES20.glVertexAttribPointer(texture, TEXCOORD_DATA_SIZE_IN_ELEMENTS, GLES20.GL_FLOAT, false,
-                0, textureBuffer);
-
-        GLES20.glEnableVertexAttribArray(texture);
-    }
 }
